4bebaa08261c32c056d0eef84c525b09c27f47cf
unknown <mikdevio>
2024-03-20 23:59:45 -0500

    Modificando estilos css dashboard

--- public/js/script.js
+++ public/js/script.js
@@ -5,74 +5,37 @@ hamBurger.addEventListener("click", function () {
   document.querySelector("#sidebar").classList.toggle("expand");
 });
 
-const ctx1 = document.getElementById("chart-1").getContext('2d');
 
-const myFirstChart = new Chart(ctx1, {
-  type:"line",
-  data: {
-    labels:["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"],
-    datasets: [{
-      label: "last week",
-      backgroundColor: "rgba(164, 198, 247, 1)",
-      borderColor: "rgb(47,128,237)",
-      data: [3000, 4200, 2000, 5100, 6420, 8520, 7352],
+let drawGraph = (typeOfGraph, canvasId, dataObject, graphOptions) => {
+  const ctx3 = document.getElementById(canvasId).getContext('2d');
+
+  const myThirdChart = new Chart(ctx3, {
+    type: typeOfGraph,
+    data: dataObject,
+    options: graphOptions
+  });
+};
+
+const graphOpt = {
+  scales: {
+    yAxes: [{
+      ticks: {
+        beginAtZero: true,
+      }
     }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true,
-        }
-      }]
-    }
   }
-});
+};
 
-const ctx2 = document.getElementById("chart-2").getContext('2d');
+const graphData = {
+  labels:["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"],
+  datasets: [{
+    label: "last week",
+    backgroundColor: "rgba(164, 198, 247, 1)",
+    borderColor: "rgb(47,128,237)",
+    data: [3000, 4200, 2000, 5100, 6420, 8520, 7352],
+  }]
+};
 
-const mySecondChart = new Chart(ctx2, {
-  type:"doughnut",
-  data: {
-    labels:["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"],
-    datasets: [{
-      label: "last week",
-      backgroundColor: "rgba(164, 198, 247, 1)",
-      borderColor: "rgb(47,128,237)",
-      data: [3000, 4200, 2000, 5100, 6420, 8520, 7352],
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true,
-        }
-      }]
-    }
-  }
-});
-
-const ctx3 = document.getElementById("chart-3").getContext('2d');
-
-const myThirdChart = new Chart(ctx3, {
-  type:"bar",
-  data: {
-    labels:["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"],
-    datasets: [{
-      label: "last week",
-      backgroundColor: "rgba(164, 198, 247, 1)",
-      borderColor: "rgb(47,128,237)",
-      data: [3000, 4200, 2000, 5100, 6420, 8520, 7352],
-    }]
-  },
-  options: {
-    scales: {
-      yAxes: [{
-        ticks: {
-          beginAtZero: true,
-        }
-      }]
-    }
-  }
-});
+drawGraph('line', "chart-1", graphData, graphOpt);
+drawGraph('doughnut', "chart-2", graphData, graphOpt);
+drawGraph('bar', "chart-3", graphData, graphOpt);
